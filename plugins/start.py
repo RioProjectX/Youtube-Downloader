d719e67a091a8f5575902bdc193487741e5764a8
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-06 23:24:48 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel 📣", url="https://t.me/rioprojects")],
-        [InlineKeyboardButton("Diskusi 📢", url="https://t.me/riogroupsupport")]
-        
-      )
-    welcomed = f"Halo <b>{message.from_user.first_name} Saya Adalah Youtube Downloader , Saya Dibuat untuk Mendownloas Video Dari Youtube</b>\n/help untuk Info Lebih Lanjut."
+        [InlineKeyboardButton("Channel 📢", url="https://t.me/rioprojects")],
+        [InlineKeyboardButton(
+            "Diskusi 📣 ", url="https://t.me/riogroupsupport")]
+    ])
+    welcomed = f"Halo <b>{message.from_user.first_name} Saya Adalah Youtube Downloader Maintaned By @fckualot , Cukup Kirim Link Dari Youtube.</b>\n/help Untuk Info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
