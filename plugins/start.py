d81b752d889d273b53d72a80f6ea4799a14ae0c8
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-06 23:17:50 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,11 +6,9 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel 📣", url="https://t.me/rioprojects")],
-        [InlineKeyboardButton(
-            "Diskusi 📢", url="https://t.me/riogroupsupport")]
-        [InlineKeyboardButton(
-            "Developer ✳", url="https://t.me/fckualot")]
-    ])
+        [InlineKeyboardButton("Diskusi 📢", url="https://t.me/riogroupsupport")]
+        [InlineKeyboardButton("Developer ✳", url="https://t.me/fckualot")]
+      )
     welcomed = f"Halo <b>{message.from_user.first_name} Saya Adalah Youtube Downloader , Saya Dibuat untuk Mendownloas Video Dari Youtube</b>\n/help untuk Info Lebih Lanjut."
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
